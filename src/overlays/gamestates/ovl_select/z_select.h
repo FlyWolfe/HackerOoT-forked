f63c84328e79ffe75a7a48e0405c5ea210e04db0
FlyWolfe <mjsherry248@gmail.com>
2026-03-04 20:23:22 -0500

    Custom_01 scene

--- src/overlays/gamestates/ovl_select/z_select.h
+++ src/overlays/gamestates/ovl_select/z_select.h
@@ -77,6 +77,7 @@ static MapSelectEntry sScenes[] = {
 #if CAN_INCLUDE_EXAMPLE_SCENE
     { "Example", MapSelect_LoadGame, ENTR_EXAMPLE_0 },
 #endif
+    { "CUSTOM_01", MapSelect_LoadGame, ENTR_CUSTOM_0 },
     { "Hyrule Field", MapSelect_LoadGame, ENTR_HYRULE_FIELD_0 },
     { "Kakariko Village", MapSelect_LoadGame, ENTR_KAKARIKO_VILLAGE_0 },
     { "Graveyard", MapSelect_LoadGame, ENTR_GRAVEYARD_0 },
